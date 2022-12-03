cfc100bc85350319e6f5cc5490725a02ce113525
ipickering <ign.pickering@gmail.com>
2022-12-03 17:06:00 -0500

    add utilities to read all inpcrd info

--- amber_utils/inpcrd.py
+++ amber_utils/inpcrd.py
@@ -1,13 +1,21 @@
 r"""Utilities to deal with amber-style inpcrd files (formatted ascii)"""
 
 from pathlib import Path
-from typing import Tuple
+from typing import Tuple, NamedTuple
 
 import pandas
 import numpy as np
 from numpy.typing import NDArray
 
 from amber_utils.utils import read_last_line
+
+
+class AmberInpcrdData(NamedTuple):
+    name: str
+    atoms_num: int
+    coordinates: NDArray[np.float_]
+    box_lengths: NDArray[np.float_]
+    box_angles: NDArray[np.float_]
 
 
 def read_coordinates_and_box(
@@ -28,3 +36,21 @@ def read_box(inpcrd: Path) -> Tuple[NDArray[np.float_], NDArray[np.float_]]:
     box_lengths = np.array(box[:3])
     box_angles = np.array(box[3:])
     return box_lengths, box_angles
+
+
+def read_name_and_num_atoms(inpcrd: Path) -> Tuple[str, int]:
+    with open(inpcrd, "r") as f:
+        for j, line in f.readlines():
+            if j == 0:
+                name = line.strip()
+            elif j == 1:
+                atoms_num = int(line.strip())
+            else:
+                break
+    return name, atoms_num
+
+
+def read_data(inpcrd: Path) -> AmberInpcrdData:
+    name, atoms_num = read_name_and_num_atoms(inpcrd)
+    coordinates, box_lengths, box_angles = read_coordinates_and_box(inpcrd)
+    return AmberInpcrdData(name, atoms_num, coordinates, box_lengths, box_angles)
