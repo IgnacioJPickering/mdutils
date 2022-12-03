6b8b2789d6b125e60f5a9dba270b2d48a44d5351
ipickering <ign.pickering@gmail.com>
2022-12-03 16:53:06 -0500

    format

--- amber_utils/utils.py
+++ amber_utils/utils.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 
 def read_last_line(file_: Path) -> str:
-    with open(file_, 'rb') as f:
+    with open(file_, "rb") as f:
         try:  # catch OSError in case of a one line file
             f.seek(-2, os.SEEK_END)
-            while f.read(1) != b'\n':
+            while f.read(1) != b"\n":
                 f.seek(-2, os.SEEK_CUR)
         except OSError:
             f.seek(0)
