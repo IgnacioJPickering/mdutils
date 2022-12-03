f3ab903ad6cfd908400783ce6e728c74b9449765
ipickering <ign.pickering@gmail.com>
2022-12-03 16:42:02 -0500

    change function name

---
+++ amber_utils/inpcrd.py
@@ -0,0 +1,19 @@
+r"""Utilities to deal with amber-style inpcrd files (formatted ascii)"""
+
+from pathlib import Path
+from typing import Tuple
+
+import pandas
+from numpy.typing import NDArray
+
+
+def read_coordinates_and_box(
+    inpcrd: Path,
+) -> Tuple[NDArray[float], NDArray[float], NDArray[float]]:
+    df = pandas.read_csv(inpcrd, skiprows=2, sep=r"\s+", header=None)
+    box = df.iloc[-1, :].values
+    df.drop(index=(df.shape[0] - 1), axis="rows", inplace=True)
+    coordinates = df.values.reshape(-1, 3)
+    box_lengths = box[:3]
+    box_angles = box[3:]
+    return coordinates, box_lengths, box_angles
