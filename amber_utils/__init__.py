45eda72282a8aedc6de670f89d2109d7363f0811
ipickering <ign.pickering@gmail.com>
2022-12-10 19:35:24 -0500

    allow imports

--- amber_utils/__init__.py
+++ amber_utils/__init__.py
@@ -1,1 +1,8 @@
 from . import cli  # noqa
+from . import inputs
+from . import inpcrd
+from . import prmtop
+from . import restrt
+from . import utils
+
+__all__ = ["inputs", "inpcrd", "prmtop", "restrt", "utils"]
