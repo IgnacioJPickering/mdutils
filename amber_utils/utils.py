e1d801fca24ba028f0d0f010e890edb1bccaff8e
ipickering <ign.pickering@gmail.com>
2022-12-10 19:29:57 -0500

    move input maker here

--- amber_utils/utils.py
+++ amber_utils/utils.py
@@ -1,5 +1,11 @@
 import os
+import math
 from pathlib import Path
+
+
+def get_dynamics_steps(time_ps: float, timestep_fs: float) -> int:
+    steps = math.ceil(time_ps * 1000 / timestep_fs)
+    return steps
 
 
 def read_last_line(file_: Path) -> str:
