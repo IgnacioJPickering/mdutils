168bbf26cb9ce83bb2dd51e5f60da5f5e6ed3a29
ipickering <ign.pickering@gmail.com>
2023-02-06 11:31:20 -0500

    mypy

--- amber_utils/implicit.py
+++ amber_utils/implicit.py
@@ -1,16 +1,12 @@
 from enum import Enum
-from typing import Optional
 
-
-# The first tuple element is the name of the "set default PBRadii" command
-# in leap, the second is the igb value in amber
-# they can be accessed with radii_name and mdin_int
 
 class ImplicitSolventModel(Enum):
     GB = "GB"
     MODIFIED_GB_I = "modified-GB-I"
     MODIFIED_GB_II = "modified-GB-II"
     GBN = "GBn"
+    MODIFIED_GBN = "modified-GBn"
     POISSON_BOLTZMANN = "PB"
     VACUUM = "vacuum"
 
@@ -19,8 +15,8 @@ _MDIN_MAP = {
     ImplicitSolventModel.GB: 1,
     ImplicitSolventModel.MODIFIED_GB_I: 2,
     ImplicitSolventModel.MODIFIED_GB_II: 5,
-    ImplicitSolventModel.GBN: 5,
-    ImplicitSolventModel.MODIFIED_GBN: 7,
+    ImplicitSolventModel.GBN: 7,
+    ImplicitSolventModel.MODIFIED_GBN: 8,
     ImplicitSolventModel.POISSON_BOLTZMANN: 10,
     ImplicitSolventModel.VACUUM: 6,
 }
