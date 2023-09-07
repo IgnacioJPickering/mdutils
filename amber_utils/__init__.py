8a74efe9670737fc7785fb9f9053a702628a9d97
ipickering <ign.pickering@gmail.com>
2023-09-07 19:42:32 -0400

    Add interface map

--- amber_utils/__init__.py
+++ amber_utils/__init__.py
@@ -3,5 +3,6 @@ from . import inpcrd
 from . import prmtop
 from . import restrt
 from . import utils
+from . import ani
 
-__all__ = ["inputs", "inpcrd", "prmtop", "restrt", "utils", "units"]
+__all__ = ["inputs", "inpcrd", "prmtop", "restrt", "utils", "units", "ani"]
