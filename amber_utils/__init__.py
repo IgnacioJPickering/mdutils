d0f79da4a33bd1915bf2a2db86fb84862821c396
Ignacio Pickering <ign.pickering@gmail.com>
2023-09-12 19:45:13 -0400

    feature/ipickering/integrate cpptraj (#38)
    * Integrate cpptra
    
    * Lint *.py with black

--- amber_utils/__init__.py
+++ amber_utils/__init__.py
@@ -4,5 +4,6 @@ from . import prmtop
 from . import restrt
 from . import utils
 from . import ani
+from . import cpptraj
 
-__all__ = ["inputs", "inpcrd", "prmtop", "restrt", "utils", "units", "ani"]
+__all__ = ["inputs", "inpcrd", "prmtop", "restrt", "utils", "units", "ani", "cpptraj"]
