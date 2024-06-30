52ffa476efd2a41f73516cf62244c5b757ff047a
Ignacio Pickering <ign.pickering@gmail.com>
2024-06-29 22:39:08 -0400

    maintain/ipickering/update cruft (#114)
    * Update cruft-json
    
    * Add tests
    
    * Remove amber_utils reference
    
    * correct import
    
    * Import amber inputs
    
    * Test prmtop reconstruction
    
    * Remove prmtop after reconstruction
    
    * remove reconstructed prmtop
    
    * Update tests
    
    * Move files
    
    * Remove old tests

--- tests/test_prmtop.py
+++ tests/test_prmtop.py
@@ -1,13 +1,14 @@
 from pathlib import Path
-import unittest
+import pytest
 
-from amber_utils import prmtop
+from mdutils.amber import prmtop
 
 
-class TestPrmtop(unittest.TestCase):
-    def testReconstructPrmtop(self) -> None:
-        expect_prmtop = (Path(__file__).parent / "test_data") / "test.prmtop"
-        data = prmtop.load(expect_prmtop)
-        result_prmtop = Path(__file__).parent / "reconstructed.prmtop"
-        prmtop.dump(data, result_prmtop, write_new_date=False)
-        self.assertEquals(expect_prmtop.read_text(), result_prmtop.read_text())
+@pytest.mark.fast
+def testReconstructPrmtop() -> None:
+    expect_prmtop = (Path(__file__).parent / "resources") / "test.prmtop"
+    data = prmtop.load(expect_prmtop)
+    result_prmtop = Path(__file__).parent / "reconstructed.prmtop"
+    prmtop.dump(data, result_prmtop, write_new_date=False)
+    assert expect_prmtop.read_text() == result_prmtop.read_text()
+    result_prmtop.unlink()
