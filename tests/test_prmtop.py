298d830a5cf869423f004c5688bc676c3662e3fc
ipickering <ign.pickering@gmail.com>
2024-06-29 22:38:25 -0400

    Update tests

--- tests/test_prmtop.py
+++ tests/test_prmtop.py
@@ -6,7 +6,7 @@ from mdutils.amber import prmtop
 
 @pytest.mark.fast
 def testReconstructPrmtop() -> None:
-    expect_prmtop = (Path(__file__).parent / "test_data") / "test.prmtop"
+    expect_prmtop = (Path(__file__).parent / "resources") / "test.prmtop"
     data = prmtop.load(expect_prmtop)
     result_prmtop = Path(__file__).parent / "reconstructed.prmtop"
     prmtop.dump(data, result_prmtop, write_new_date=False)
