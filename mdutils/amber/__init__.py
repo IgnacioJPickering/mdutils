a020a54f5f165eca09440be68047b2f476d685ce
ipickering <ign.pickering@gmail.com>
2024-10-20 17:52:15 -0400

    Make Prmtop, ... importable from mdutils.amber directly

---
+++ mdutils/amber/__init__.py
@@ -0,0 +1,11 @@
+from mdutils.amber.prmtop import Prmtop, PrmtopMeta
+from mdutils.amber.restart import Restart, RestartMeta
+from mdutils.amber.inpcrd import Inpcrd, InpcrdMeta
+__all__ = [
+    "Prmtop",
+    "PrmtopMeta",
+    "Restart",
+    "RestartMeta",
+    "Inpcrd",
+    "InpcrdMeta",
+]
