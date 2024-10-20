736d08511cdbc94c984c3e6259b5d23142afadd7
ipickering <ign.pickering@gmail.com>
2024-10-20 17:52:28 -0400

    black

--- mdutils/amber/__init__.py
+++ mdutils/amber/__init__.py
@@ -1,6 +1,7 @@
 from mdutils.amber.prmtop import Prmtop, PrmtopMeta
 from mdutils.amber.restart import Restart, RestartMeta
 from mdutils.amber.inpcrd import Inpcrd, InpcrdMeta
+
 __all__ = [
     "Prmtop",
     "PrmtopMeta",
